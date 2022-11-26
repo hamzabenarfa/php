978ce44c35a6e5e8e7cad6ee12059a6c0e41242a
hamza benarfa <hamzabenarfa4@gmail.com>
2022-11-26 23:01:30 +0100

    version 1.0

---
+++ 1.0/ajouter.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Ajouter</title>
+</head>
+<body>
+    <form  method="get">>
+
+        
+
+    </form>
+
+
+    
+</body>
+</html>
