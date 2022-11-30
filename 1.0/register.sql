7066de6a092431c58ed72d431ec09b9879a98c10
hamza <hamzabenarfa4@gmail.com>
2022-11-30 19:05:24 +0100

    updated sql

--- 1.0/register.sql
+++ 1.0/register.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2022 at 12:10 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Generation Time: Nov 30, 2022 at 07:03 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,19 +28,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `register` (
+  `id` int(10) NOT NULL,
   `mail` varchar(30) NOT NULL,
   `nom` varchar(30) NOT NULL,
   `prenom` varchar(30) NOT NULL,
   `age` int(3) NOT NULL,
   `pwd` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `register`
 --
 
-INSERT INTO `register` (`mail`, `nom`, `prenom`, `age`, `pwd`) VALUES
-('hamza', 'ee', 'ee', 0, 'aze');
+INSERT INTO `register` (`id`, `mail`, `nom`, `prenom`, `age`, `pwd`) VALUES
+(4, 'hamza', '', '', 0, 'hamza');
 
 --
 -- Indexes for dumped tables
@@ -50,7 +51,17 @@ INSERT INTO `register` (`mail`, `nom`, `prenom`, `age`, `pwd`) VALUES
 -- Indexes for table `register`
 --
 ALTER TABLE `register`
-  ADD PRIMARY KEY (`mail`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `register`
+--
+ALTER TABLE `register`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
